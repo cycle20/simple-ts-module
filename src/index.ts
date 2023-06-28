35dd61497b49ca715f219c4a322a6d33fc1eca70
cycle20 <46718432+cycle20@users.noreply.github.com>
2023-06-28 15:02:46 +0200

    This works in Chrome and FF

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,16 @@
 //import './index.html';
 
-console.log('Hello');
-
 class X {
 
     public loadRegionColorsFromStyleSheets(prefix: string = 'ult_') {
-        const sheets: CSSStyleSheet[] = Array.from(document.styleSheets); //.forEach(x => console.log(x));
+        const sheets: CSSStyleSheet[] = Array.from(document.styleSheets);
         sheets.forEach((sheet: CSSStyleSheet) => {
             const rules: CSSRule[] = Array.from(sheet.cssRules);
             rules.forEach((rule: CSSRule) => {
                 let r = rule as CSSStyleRule;
-                // console.log(r);
-                if (rule.cssText.startsWith('.' + prefix)) {
-                    console.log(rule.cssText);
+                if (r.selectorText?.startsWith('.' + prefix)) {
+                    console.log(r.style.getPropertyValue('fill'));
                 }
-                // if (r.selectorText.startsWith('.' + prefix)) {
-                //     console.log(r.style.getPropertyValue('fill'));
-                // }
             });
         });
     }
