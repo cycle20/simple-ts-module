7f9ff201969dd8075966e627fc8bf0e40502d5d5
cycle20 <46718432+cycle20@users.noreply.github.com>
2023-06-28 15:12:33 +0200

    Refactoring: fetchRule function added

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,15 @@ class X {
         const sheets: CSSStyleSheet[] = Array.from(document.styleSheets);
         sheets.forEach((sheet: CSSStyleSheet) => {
             const rules: CSSRule[] = Array.from(sheet.cssRules);
-            rules.forEach((rule: CSSRule) => {
-                let r = rule as CSSStyleRule;
-                if (r.selectorText?.startsWith('.' + prefix)) {
-                    console.log(r.style.getPropertyValue('fill'));
-                }
-            });
+            rules.forEach((rule: CSSRule) => this.fetchRule(rule, prefix));
         });
+    }
+
+    private fetchRule(rule: CSSRule, prefix: string) {
+        let styleRule = rule as CSSStyleRule;
+        if (styleRule.selectorText?.startsWith('.' + prefix)) {
+            console.log(styleRule.style.getPropertyValue('fill'));
+        }
     }
 }
 
